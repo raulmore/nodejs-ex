621ed3e26ed01175dfdf6d49e0d0eb54e7821659
raul <raul.moreno04@gmail.com>
2017-10-11 13:25:33 +0200

    Test

--- server.js
+++ server.js
@@ -74,6 +74,10 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/views/:name', function (req, res) {
+	res.sendFile(req.params.name);
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
