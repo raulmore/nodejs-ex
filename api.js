c5ee093bbb8f8f034ca781142b9967e51d5a9f08
raul <raul.moreno04@gmail.com>
2018-02-16 10:06:59 +0100

    Test good proto 5?

--- api.js
+++ api.js
@@ -1,30 +1,5 @@
 
-//const URL = 'mongodb://localhost:27017/geodocuments';
-
-var URL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-	mongoURLLabel = "";
-
-if (URL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = URL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      URL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    URL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-  }
-}
-
-
-
+const URL = 'mongodb://localhost:27017/geodocuments';
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 
