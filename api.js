92314de5dcad02285df82ffb865ee026956b7feb
raul <raul.moreno04@gmail.com>
2018-02-16 12:48:13 +0100

    Test good proto 5?

--- api.js
+++ api.js
@@ -1,5 +1,21 @@
 
-const URL = 'mongodb://localhost:27017/geodocuments';
+var URL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+if (URL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    URL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      URL += mongoUser + ':' + mongoPassword + '@';
+    }
+    URL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+}
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 
